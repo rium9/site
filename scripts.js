eaa1c425826363379e63fa2d81407ba7cb59badf
Jamie <jamiemuir16@gmail.com>
2026-05-12 22:54:41 +0100

    Cleanup script

--- scripts.js
+++ scripts.js
@@ -1,16 +1,28 @@
-document.addEventListener('click', (event) => {
-    const main = document.getElementById('main');
-    const div = document.createElement('button');
+function rippleClickHandler(event) {
+    const CIRCLE_RADIUS = 10;
+    const CIRCLE_DIAMETER = CIRCLE_RADIUS * 2;
+
+    const main = document.getElementsByTagName("main")[0];
+    const div = document.createElement("div");
 
     div.className = "circle";
-    div.style.position = 'absolute';
-    div.style.top = (event.clientY - 10) + "px";
-    div.style.left = (event.clientX - 10) + "px";
+    div.style.position = "absolute";
+
+    div.style.width  = `${CIRCLE_DIAMETER}px`;
+    div.style.height = `${CIRCLE_DIAMETER}px`;
+
+    div.style.top = `${event.clientY - CIRCLE_RADIUS}px`;
+    div.style.left = `${event.clientX - CIRCLE_RADIUS}px`;
 
     main.appendChild(div);
 
     setTimeout(() => {
         main.removeChild(div);
-    }, 2000);
+    }, 1000);
+}
 
-})
+function addRippleListener() {
+    document.addEventListener('click', rippleClickHandler);
+}
+
+addRippleListener();
